9e97f462e15797b7e3fe227adb25d94db80f6c63
hossein <hosseinjahanbanifar@gmail.com>
2024-12-10 20:56:59 +0330

    style: Add some styles to about and home section

--- portfolio.js
+++ portfolio.js
@@ -1,20 +1,19 @@
 
-const backToTopButton = document.getElementById('back-to-up');
+document.addEventListener('DOMContentLoaded', () => {
+    const backToTopButton = document.getElementById('backToUp');
+    
 
-// Show the button when the user scrolls down 200px
-window.addEventListener('scroll', () => {
-    // if (window.scrollY > 200) {
-    //   backToTopButton.style.display = 'block';
-    // } else {
-    //   backToTopButton.style.display = 'none';
-    // }
-    backToTopButton.style.display = window.scrollY > 200 ? 'block' : 'none'; 
-});
-
-// Scroll to the top when the button is clicked
-backToTopButton.addEventListener('click', () => {
-    window.scrollTo({
-      top: 0,        // Scroll to the top of the page
-      behavior: 'smooth' // Smooth scrolling effect
+    // Show the button when the user scrolls down 200px
+    window.addEventListener('scroll', () => {
+        backToTopButton.style.display = window.scrollY>200 ? 'block' : 'none';
     });
-});
+
+
+    // Scroll to the top when the button is clicked
+    backToTopButton.addEventListener('click', () => {
+        window.scrollTo({
+            top: 0,   
+            behavior: 'smooth' 
+        });
+    });
+})
